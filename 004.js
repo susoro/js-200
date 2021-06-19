3821e6e7b92eec8474896228e6f583593f71a896
susoro <susoro567@gmail.com>
2021-06-19 23:49:16 +0900

    공부시작

---
+++ 004.js
@@ -0,0 +1,4 @@
+console.log( `vs coede 로 코드 실행해보기 2`);
+var a = 5; 
+var b = 19;
+console.log(a + b);
---
+++ common.js
@@ -0,0 +1,14 @@
+console.log('vscode 로 실행해보기');
+var a = 5;
+var b = 6;
+console.log(a + b);
+
+
+function greeting() {
+  `hello`
+  `chloe` + 3
+  greeting()
+}
+greeting(if(true){
+  console.log(`it is not acceptable`) 
+})
