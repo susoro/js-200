1671fc3c2311f8472918a759c69c6089f455d5e3
susoro <susoro567@gmail.com>
2021-06-20 20:19:27 +0900

    Create 023.js

---
+++ 023.js
@@ -0,0 +1,19 @@
+// and 논리 연산자 &&
+/*
+console.log(true && true);
+console.log(true && false);
+console.log(`문장` == `문장` && 5 == 5);
+console.log(5==5 && `다른문장 1` == `다른문장 2`);
+
+// or 논리 연산자 ||
+console.log(true || false);
+console.log(false || false);
+console.log(`문장`== `문장` || 5 ==10);
+*/
+// not논리 연산자 !
+console.log(!true); //false
+console.log(!false); //true
+console.log(!5); // false
+console.log(!`문장`); //false
+console.log(!!5); //true
+console.log(!!`문장`); //true
