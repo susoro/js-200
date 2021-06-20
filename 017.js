6540f7c826070147d373ba4d4d5bc86f56d3cd60
susoro <susoro567@gmail.com>
2021-06-20 18:35:11 +0900

    🥰

---
+++ 017.js
@@ -0,0 +1,4 @@
+console.log( `I'm in seoul`);
+console.log(`Sewha ocean is wonderful`);
+console.log(`have you ever eaten makgeolli?`);
+console.log(`this is the first line\nand this is the second`);
