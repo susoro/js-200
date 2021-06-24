237839d649364ed94da692fe174346fddfe0049e
susoro <susoro567@gmail.com>
2021-06-25 00:49:54 +0900

    공부

---
+++ common02.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+// variable  변경될수있는값
+
+let globalName = 'globalname';
+
+{let name = 'su gyeong';
+console.log(name);
+name = 'hihihi';
+console.log(name);}
+
+console.log(globalName);
+
+//const 값이 변하지 않는것
